afeba3923d70754a2bda4120335698f34c5f361c
tesstryan <tesstryan@gmail.com>
2013-12-11 10:39:59 -0600

    Updated Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ def create
   shelter = Shelter.find_by_email(params[:email])
   if shelter.present? && shelter.authenticate(params[:password])
     session[:shelter_id] = shelter.id
+
     redirect_to shelter_url(shelter.id)
   else
     redirect_to new_session_url, notice: "Email/Password are incorrect. Please try again."
